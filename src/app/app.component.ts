87ddd3d00b13302a2df43f273a0a7baaec327af6
Arjun Chib <arjunpatrickchib@gmail.com>
2023-07-22 02:37:19 -0400

    Add search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
-declare global {
-  var SONGS: any[];
-}
+import { songs } from './data/songs';
+import { FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rockhero';
+  songs = songs;
+  songVectors = songs.map((song) =>
+    Object.values(song).join(' ').toLowerCase()
+  );
+  search = new FormControl('');
+
+  ngOnInit(): void {
+    this.search.valueChanges.pipe(debounceTime(300)).subscribe((value) => {
+      value ? this.filterSongs(value) : (this.songs = songs);
+    });
+  }
+
+  private filterSongs(value: string) {
+    this.songs = songs.filter((song, i) =>
+      this.songVectors[i].includes(value.toLowerCase())
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { SongsComponent } from './songs/songs.component';
 
 @NgModule({
-  declarations: [AppComponent, SongsComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
